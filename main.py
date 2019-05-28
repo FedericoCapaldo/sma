f1cafd0eab7e86adeb4a59e8aebd68ccdde5e9ad
FedericoKCL <federico.capaldo1@gmail.com>
2019-05-28 20:45:38 +0100

    of course it doesnt work yet, but I put time into it.

--- main.py
+++ main.py
@@ -18,28 +18,78 @@ def get_forex_data(file_name):
 class SMA_val:
     def __init__(self, name):
         self.name = name
-        self.days = 0
         self.daily_values = []
+
+    def get_current_price(self):
+        return float(self.daily_values[-1][1])
 
     def add_daily_quote(self, today_val):
         self.daily_values.append(today_val)
-        self.days = self.days + 1
 
-    def current_sma(self):
+    def sma_50(self):
         if len(self.daily_values) == 0:
             print("no values to calculate sma")
             return 0
-        return reduce((lambda x, y: x + y), self.daily_values) / self.days
+        elif len(self.daily_values) < 50:
+            print("no enough values to calculate sma")
+            return 0
+
+        return reduce((lambda x, y: x + y), map((lambda x: float(x[1])),self.daily_values[-50:])) / 50
+
+    def sma_20(self):
+        if len(self.daily_values) == 0:
+            print("no values to calculate sma")
+            return 0
+        elif len(self.daily_values) < 20:
+            print("no enough values to calculate sma")
+            return 0
+
+        return reduce((lambda x, y: x + y), map((lambda x: float(x[1])),self.daily_values[-20:])) / 20
+
+
+def is_moving_average_below(currency):
+    return currency.sma_20() < currency.sma_50()
+
+
+def sell(position, money, eur, gbp):
+    if position == eur.name:
+        return money * gbp.get_current_price()
+    elif position == gbp.name:
+        return money * eur.get_current_price()
 
 
 if __name__ == "__main__":
-    tuple_date_eurval = get_forex_data(EUR_FILE)
-    tuple_date_gbpval = get_forex_data(GBP_FILE)
+    eur_vals = get_forex_data(EUR_FILE)
+    gbp_vals = get_forex_data(GBP_FILE)
 
-    DAY = 1
-    euro = SMA_val("eur")
+    MONEY = 1000
+    DAY = 0
+    eur = SMA_val("eur")
     gbp = SMA_val("gbp")
 
+    position = "gbp"
+
     while True:
-        # something
-    
+        eur.add_daily_quote(eur_vals[DAY])
+        gbp.add_daily_quote(gbp_vals[DAY])
+        DAY += 1
+        if DAY < 50:
+            continue
+
+        if position == "gbp":
+            if is_moving_average_below(gbp):
+                MONEY = sell(position, MONEY, eur, gbp)
+                position = "eur"
+            # else:
+                # nothing for now
+        elif position == "eur":
+            if is_moving_average_below(eur):
+                MONEY = sell(position, MONEY, eur, gbp)
+                position = "gbp"
+            # else:
+                # nothing for now
+
+        if DAY > 3000 and position == "gbp":
+            print(MONEY)
+            print(DAY)
+            break
